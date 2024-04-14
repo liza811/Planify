c2382b760d60a2b4be8cfb824284476993e7c083
liza811 <150456396+liza811@users.noreply.github.com>
2024-04-14 19:57:50 +0100

    Update notifications.tsx

--- app/u/_components/notifications.tsx
+++ app/u/_components/notifications.tsx
@@ -69,7 +69,7 @@ export const Notifications = ({ mesNotifications }: notificationsProps) => {
                     key={notification.id}
                     className="flex w-full  gap-x-2 bg-white rounded-md b relative "
                   >
-                    <div className="bg-primary_purpule w-[4px] h-full  overflow-hidden rounded-md absolute right-0 top-0" />
+                    <div className="bg-primary_purpule w-[4px] h-full  overflow-hidden rounded-md absolute left-0 top-0" />
                     <div className="bg-neutral-100    rounded-full flex justify-center items-center p-2 h-fit mt-4">
                       <Bell size={17} className="text-primary_blue " />
                     </div>
