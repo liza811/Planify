ba8ef13ca5c8d865a69725c8a67cbe809f329103
liza811 <150456396+liza811@users.noreply.github.com>
2024-04-17 20:48:51 +0100

    Update page.tsx

--- app/u/etudiant/[name]/meschoix/page.tsx
+++ app/u/etudiant/[name]/meschoix/page.tsx
@@ -1,4 +1,11 @@
+import { Theme } from "@/components/etudiant";
+
 import Intermidiere from "./intermidiere";
+import { $Enums } from "@prisma/client";
+
+export interface ExtendedTheme extends Theme {
+  etat?: $Enums.Etat;
+}
 
 const ChoixPage = async () => {
   return <Intermidiere />;
