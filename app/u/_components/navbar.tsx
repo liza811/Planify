4f933865a49bca638281149f06464a471d1e939b
liza811 <150456396+liza811@users.noreply.github.com>
2024-04-23 22:26:31 +0100

    Update navbar.tsx

--- app/u/_components/navbar.tsx
+++ app/u/_components/navbar.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from "@/components/auth/user-button";
 import { MobileSidebar } from "./mobile-sidebar";
-import { getDepartement } from "@/lib/departement";
+
 
 export const Navbar = async () => {
   const departement = await getDepartement();
@@ -8,11 +8,8 @@ export const Navbar = async () => {
     <div className="border-b p-4 bg-white shadow-sm flex items-center md:flex-row-reverse justify-between h-full w-full">
       <MobileSidebar />
       <UserButton />
-      {!!departement && (
-        <h1 className="text-lg font-bold text-primary_blue mx-4 md:text-[20px]">
-          Département {departement.nom}
-        </h1>
-      )}
+     
+   
     </div>
   );
 };
