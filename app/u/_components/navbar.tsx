21180ca30b00f2396723e98ee5fff0cae44d495e
liza811 <150456396+liza811@users.noreply.github.com>
2024-04-23 22:17:53 +0100

    navbar.tsx

--- app/u/_components/navbar.tsx
+++ app/u/_components/navbar.tsx
@@ -1,19 +1,18 @@
 import { UserButton } from "@/components/auth/user-button";
 import { MobileSidebar } from "./mobile-sidebar";
-import { Notifications } from "./notifications";
-import { getNotificationsByUser } from "@/lib/notifications";
+import { getDepartement } from "@/lib/departement";
 
 export const Navbar = async () => {
-  const notificaions = await getNotificationsByUser();
-
+  const departement = await getDepartement();
   return (
     <div className="border-b p-4 bg-white shadow-sm flex items-center md:flex-row-reverse justify-between h-full w-full">
       <MobileSidebar />
-      <div className="flex gap-x-6 items-center">
-        <Notifications mesNotifications={notificaions} />
-
-        <UserButton />
-      </div>
+      <UserButton />
+      {!!departement && (
+        <h1 className="text-lg font-bold text-primary_blue mx-4 md:text-[20px]">
+          Département {departement.nom}
+        </h1>
+      )}
     </div>
   );
 };
